72cec92833d16e54c098d7b40fcc6fbf2e1227df
Ryan McDermott <ryantmcdermott@gmail.com>
2021-05-22 16:54:44 -0700

    Add empty program parser

--- src/parser.rs
+++ src/parser.rs
@@ -1,46 +1,69 @@
 #![allow(dead_code)]
-use crate::tokenizer::Token;
+use crate::tokenizer::{Token, Tokenizer};
 
 #[derive(Debug, Clone, PartialEq, Eq)]
-pub enum LiteralKind {
-    Number,
-    String,
-}
+pub struct ExpressionStatement {}
 
 #[derive(Debug, Clone, PartialEq, Eq)]
-pub enum LiteralValue {
-    Number(i64),
-    String(String),
+pub enum Node {
+    ExpressionStatement(ExpressionStatement),
 }
 
 #[derive(Debug, Clone, PartialEq, Eq)]
-pub struct Literal {
-    pub kind: LiteralKind,
-    pub value: LiteralValue,
-}
-
-pub struct Expression {}
-
-pub enum NodeType {
-    Expression(Expression),
-}
-
-pub struct Node {
-    node_type: NodeType,
+pub struct Program {
+    node_type: String,
     body: Vec<Node>,
 }
 
 pub struct Parser {
-    tokens: Vec<Token>,
+    tokenizer: Tokenizer,
+    lookahead: Option<Token>,
 }
 
 impl Parser {
-    fn parse(&mut self) {}
+    pub fn new(mut tokenizer: Tokenizer) -> Parser {
+        let lookahead = tokenizer.get_next_token();
+        Parser {
+            tokenizer: tokenizer,
+            lookahead: lookahead,
+        }
+    }
+
+    fn parse(&mut self) -> Program {
+        match &self.lookahead {
+            None => {
+                return Program {
+                    node_type: "Program".to_string(),
+                    body: Vec::new(),
+                }
+            }
+            Some(_) => {
+                return Program {
+                    node_type: "Program".to_string(),
+                    body: self.parse_statement_list(),
+                }
+            }
+        };
+    }
+
+    fn parse_statement_list(&self) -> Vec<Node> {
+        Vec::new()
+    }
 }
 
 #[cfg(test)]
 mod tests {
-    // use super::*;
+    use super::*;
     #[test]
-    fn numeric_literal() {}
+    fn empty_program() {
+        let tokenizer = Tokenizer::new("".to_string());
+        let mut parser = Parser::new(tokenizer);
+        let actual = parser.parse();
+        let expected = Program {
+            node_type: "Program".to_string(),
+            body: vec![],
+        };
+
+        assert_eq!(actual, expected);
+    }
 }
