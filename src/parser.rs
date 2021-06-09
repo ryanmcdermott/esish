352655349caef90d4238e26e07dd97ab5590de8d
Ryan McDermott <ryantmcdermott@gmail.com>
2021-06-09 10:17:32 -0700

    Add README example

--- src/parser.rs
+++ src/parser.rs
@@ -2675,4 +2675,235 @@ mod tests {
 
         expect_ast!(program, expected);
     }
+
+    #[test]
+    fn readme_example() {
+        let program = r#"
+          class Fib {
+            function calc(num) {
+              if (num <= 1) {
+                return 1;
+              }
+
+              return this.calc(num - 1) + this.calc(num - 2);
+            }
+          }
+
+          let fib = new Fib();
+          fib.calc(42);
+        "#
+        .to_string();
+        let expected = r#"
+        {
+          "Program": {
+            "body": [
+              {
+                "ClassDeclaration": {
+                  "id": {
+                    "name": "Fib"
+                  },
+                  "body": {
+                    "body": [
+                      {
+                        "FunctionDeclaration": {
+                          "name": {
+                            "name": "calc"
+                          },
+                          "params": [
+                            {
+                              "name": "num"
+                            }
+                          ],
+                          "body": {
+                            "body": [
+                              {
+                                "IfStatement": {
+                                  "test": {
+                                    "BinaryExpression": {
+                                      "left": {
+                                        "Identifier": {
+                                          "name": "num"
+                                        }
+                                      },
+                                      "right": {
+                                        "Literal": {
+                                          "NumericLiteral": {
+                                            "value": 1
+                                          }
+                                        }
+                                      },
+                                      "operator": "OperatorRelational"
+                                    }
+                                  },
+                                  "consequent": {
+                                    "BlockStatement": {
+                                      "body": [
+                                        {
+                                          "ReturnStatement": {
+                                            "argument": {
+                                              "Literal": {
+                                                "NumericLiteral": {
+                                                  "value": 1
+                                                }
+                                              }
+                                            }
+                                          }
+                                        }
+                                      ]
+                                    }
+                                  },
+                                  "alternate": null
+                                }
+                              },
+                              {
+                                "ReturnStatement": {
+                                  "argument": {
+                                    "BinaryExpression": {
+                                      "left": {
+                                        "CallExpression": {
+                                          "callee": {
+                                            "MemberExpression": {
+                                              "object": {
+                                                "ThisExpression": {}
+                                              },
+                                              "computed": false,
+                                              "property": {
+                                                "Identifier": {
+                                                  "name": "calc"
+                                                }
+                                              }
+                                            }
+                                          },
+                                          "arguments": [
+                                            {
+                                              "BinaryExpression": {
+                                                "left": {
+                                                  "Identifier": {
+                                                    "name": "num"
+                                                  }
+                                                },
+                                                "right": {
+                                                  "Literal": {
+                                                    "NumericLiteral": {
+                                                      "value": 1
+                                                    }
+                                                  }
+                                                },
+                                                "operator": "OperatorAdd"
+                                              }
+                                            }
+                                          ]
+                                        }
+                                      },
+                                      "right": {
+                                        "CallExpression": {
+                                          "callee": {
+                                            "MemberExpression": {
+                                              "object": {
+                                                "ThisExpression": {}
+                                              },
+                                              "computed": false,
+                                              "property": {
+                                                "Identifier": {
+                                                  "name": "calc"
+                                                }
+                                              }
+                                            }
+                                          },
+                                          "arguments": [
+                                            {
+                                              "BinaryExpression": {
+                                                "left": {
+                                                  "Identifier": {
+                                                    "name": "num"
+                                                  }
+                                                },
+                                                "right": {
+                                                  "Literal": {
+                                                    "NumericLiteral": {
+                                                      "value": 2
+                                                    }
+                                                  }
+                                                },
+                                                "operator": "OperatorAdd"
+                                              }
+                                            }
+                                          ]
+                                        }
+                                      },
+                                      "operator": "OperatorAdd"
+                                    }
+                                  }
+                                }
+                              }
+                            ]
+                          }
+                        }
+                      }
+                    ]
+                  },
+                  "super_class": null
+                }
+              },
+              {
+                "VariableStatement": {
+                  "declarations": [
+                    {
+                      "id": {
+                        "name": "fib"
+                      },
+                      "init": {
+                        "NewExpression": {
+                          "callee": {
+                            "Identifier": {
+                              "name": "Fib"
+                            }
+                          },
+                          "arguments": []
+                        }
+                      }
+                    }
+                  ]
+                }
+              },
+              {
+                "ExpressionStatement": {
+                  "expression": {
+                    "CallExpression": {
+                      "callee": {
+                        "MemberExpression": {
+                          "object": {
+                            "Identifier": {
+                              "name": "fib"
+                            }
+                          },
+                          "computed": false,
+                          "property": {
+                            "Identifier": {
+                              "name": "calc"
+                            }
+                          }
+                        }
+                      },
+                      "arguments": [
+                        {
+                          "Literal": {
+                            "NumericLiteral": {
+                              "value": 42
+                            }
+                          }
+                        }
+                      ]
+                    }
+                  }
+                }
+              }
+            ]
+          }
+        }
+        "#
+        .to_string();
+
+        expect_ast!(program, expected);
+    }
 }
